36ef749672d0a0561042141201c10b5896704c4d
lalitkumarthakur <lkt929129@gmail.com>
2023-08-01 20:37:54 +0530

    Updated notes for java JVM main method

--- src/basics/hello.java
+++ src/basics/hello.java
@@ -11,8 +11,9 @@
 
 
 // JVM executes only one file besides how many files are there in the project.
-// and that file needs to specified by the programmer and it needs to contain main method. That main medtod file is executed by JVM.
-
+// and that file needs to specified by the programmer and it needs to contain main method. That main method file is executed by JVM.
+// Also main method needs some more words to be ececuted( public static void) amd main() accepts an argument (ex- main(String args[])).
+// A class is also required to execute main() method. EX- public class className {}.
 
 package basics;
 
