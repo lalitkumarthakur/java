548d9950208b2d1033c58be4fe684f7664379bdc
lalitkumarthakur <lkt929129@gmail.com>
2023-08-01 18:59:12 +0530

    Added notes for java code processing

--- src/basics/hello.java
+++ src/basics/hello.java
@@ -1,5 +1,14 @@
 // Notes
 
+// Our computer does not understand java code directly so it needs to be converted.
+// That why our compiler converts Java code into Byte code so that JVM (Java Virtual Machine)
+// can understand and make our OS then hardware understand the code and perform our desired output.
+// Refer to this diagram below - 
+
+// We(source code) -> Compiler ->
+// Byte Code -> JVM (Java Virtual Machine)
+// OS -> Hardware -> Return back.
+
 package basics;
 
 public class hello {
